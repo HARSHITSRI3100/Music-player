79755d646834ec5f696fdb77deaa40df083e90bc
HARSHITSRI3100 <140917595+HARSHITSRI3100@users.noreply.github.com>
2024-02-13 21:58:38 +0530

    Add files via upload

---
+++ musicplayer.py
@@ -0,0 +1,61 @@
+import pygame
+from pygame import mixer
+from tkinter import *
+import os
+
+def playsong():
+    currentsong=playlist.get(ACTIVE)
+    print(currentsong)
+    mixer.music.load(currentsong)
+    songstatus.set("Playing")
+    mixer.music.play()
+
+def pausesong():
+    songstatus.set("Paused")
+    mixer.music.pause()
+
+def stopsong():
+    songstatus.set("Stopped")
+    mixer.music.stop()
+
+def resumesong():
+    songstatus.set("Resuming")
+    mixer.music.unpause()
+
+root=Tk()
+root.title(' Mp3 Music Player')
+
+mixer.init()
+songstatus=StringVar()
+songstatus.set("choosing")
+
+#playlist---------------
+
+playlist = Listbox(root, selectmode=SINGLE, bg="ivory", fg="black", font=('arial', 15), width=40)
+playlist.grid(columnspan=5)
+
+os.chdir(r'C:\Users\Harshit Srivastava\Downloads\1')
+songs = os.listdir()
+for s in songs:
+    playlist.insert(END, s)
+
+playbtn = Button(root, text="Play", command=playsong)
+playbtn.config(font=('arial', 18), bg="darkslategray", fg="white", padx=7, pady=7)
+playbtn.grid(row=1, column=0)
+
+pausebtn = Button(root, text="Pause", command=pausesong)
+pausebtn.config(font=('arial', 18), bg="darkslategray", fg="white", padx=7, pady=7)
+pausebtn.grid(row=1, column=1)
+
+Resumebtn = Button(root, text="Resume", command=resumesong)
+Resumebtn.config(font=('arial', 18), bg="darkslategray", fg="white", padx=7, pady=7)
+Resumebtn.grid(row=1, column=2)
+
+stopbtn = Button(root, text="Stop", command=stopsong)
+stopbtn.config(font=('arial', 18), bg="darkslategray", fg="white", padx=7, pady=7)
+stopbtn.grid(row=1, column=3)
+
+
+
+
+mainloop()
